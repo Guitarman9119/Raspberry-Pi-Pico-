ae4c94f670e292456f5908ab0c56984d5950f297
Vernon Peens <vwpeens@gmail.com>
2022-04-09 15:03:19 +0800

    Create code.py

--- DS1302 RTC/code.py
+++ DS1302 RTC/code.py
@@ -1,10 +1,13 @@
 """
 
 DS1302 real-time clock module
-Special thanks to yunline for the Library: https://github.com/omarbenhamid/micropython-ds1302-rtc
+
+Special thanks to yunline for the DS1302  Library: https://github.com/omarbenhamid/micropython-ds1302-rtc
+and T-622 for the I2C LCD library: https://github.com/T-622/RPI-PICO-I2C-LCD
+
 Remember to check out more tutorials on NerdCave - https://www.youtube.com/c/NerdCaveYT
 
-Pinout
+Project Pinout
 VCC - VSYS (PIN39)
 GND - GND (Any ground on Pico)
 CLK - GP18 (PIN24)
@@ -13,12 +16,10 @@ RST  - GP16 (PIN21)
 
 """
 
-import utime
-import ds1302
-from machine import I2C, Pin
-from lcd_api import LcdApi
-from pico_i2c_lcd import I2cLcd
 
+from machine import I2C, Pin
+from ds1302 import DS1302
+from pico_i2c_lcd import I2cLcd
 
 I2C_ADDR     = 63
 I2C_NUM_ROWS = 2
@@ -27,41 +28,35 @@ I2C_NUM_COLS = 16
 i2c = I2C(0, sda=machine.Pin(0), scl=machine.Pin(1), freq=400000)
 lcd = I2cLcd(i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
 
-
-
-ds = ds1302.DS1302(Pin(18),Pin(17),Pin(16))
+ds = DS1302(Pin(18),Pin(17),Pin(16))
 
 ds.date_time() # returns the current datetime.
-ds.date_time([2023, 3, 2, 0, 8, 17, 50, 0]) # set datetime.
 
-print(ds.date_time())
+#ds.date_time([2023, 3, 2, 0, 8, 17, 50, 0]) # set datetime.
 
-
-while True:
-    
-    lcd.clear()
-    lcd.putstr(str(ds.year()) + "/" +  str(ds.month()) + "/" + str(ds.day()) + " " + str(ds.hour()) + ":" + str(ds.minute()) + ":" + str(ds.second()))
-    utime.sleep(1)
-
-    
-    
-'''
-rtc=machine.RTC()
-rtc.datetime((2020, 1, 21, 2, 10, 32, 36, 0))
-
+#ds.hour() # returns hour.
+#print(ds.date_time())
 
 
 while True:
-    reading = sensor_temp.read_u16() * conversion_factor
-    timestamp=rtc.datetime()
-    temperature = 27 - (reading - 0.706)/0.001721
-
-    timestring="%04d-%02d-%02d %02d:%02d:%02d"%(timestamp[0:3] +
-                                                timestamp[4:7])
-    print(timestring + "," + str(temperature) + "\n")
-
-    led_onboard.value(1)
-    utime.sleep(0.01)
-    led_onboard.value(0)
-'''
-
+    (Y,M,D,day,hr,m,s)=ds.date_time()
+    if s < 10:
+        s = "0" + str(s)
+    if m < 10:
+        m = "0" + str(m)
+    if hr < 10:
+        hr = "0" + str(hr)
+    if D < 10:
+        D = "0" + str(D)
+    if M < 10:
+        hr = "0" + str(M)
+        
+    lcd.move_to(0,0)
+    lcd.putstr("Time:")
+    lcd.move_to(6,0)
+    lcd.putstr(str(hr) + ":" + str(m) + ":" + str(s))
+    lcd.move_to(0,1)
+    lcd.putstr("Date:")
+    lcd.move_to(6,1)
+    lcd.putstr(str(D) + "/" + str(M) + "/" + str(Y))
+   
