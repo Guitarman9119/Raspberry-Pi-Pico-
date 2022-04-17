7a10039e9c750f96b1a89b1357df4f27152e3893
Vernon Peens <vwpeens@gmail.com>
2022-04-17 10:35:35 +0800

    Update animation.py

--- SSD1306 OLED Display/animation.py
+++ SSD1306 OLED Display/animation.py
@@ -3,18 +3,21 @@ from ssd1306 import SSD1306_I2C
 import framebuf
 import time
 
+WIDTH = 128
+HEIGHT = 64
+
 i2c = I2C(0, scl = Pin(17), sda = Pin(16), freq=400000)
 display = SSD1306_I2C(WIDTH, HEIGHT, i2c)
 
 
 images = []
 for n in range(1, x):
-    with open('/folder/image%s.pbm' % n, 'rb') as f:
+    with open('/folder/image%s.pbm' % n, 'rb') as f:  #open folder and image
         f.readline() # Magic number
         f.readline() # Creator comment
         f.readline() # Dimensions
         data = bytearray(f.read())
-    fbuf = framebuf.FrameBuffer(data, 128, 64, framebuf.MONO_HLSB)
+    fbuf = framebuf.FrameBuffer(data, 128, 64, framebuf.MONO_HLSB) #adjust accordingly the width and height
     images.append(fbuf)
     
 
